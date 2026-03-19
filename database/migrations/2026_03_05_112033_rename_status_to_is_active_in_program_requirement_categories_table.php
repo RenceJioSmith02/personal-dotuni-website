7e08c19f346f7485fea7b7fd22469a0f47b9b519
dannnverrrr <166786008+dannnverrrr@users.noreply.github.com>
2026-03-19 16:26:30 +0800

    migration error

--- database/migrations/2026_03_05_112033_rename_status_to_is_active_in_program_requirement_categories_table.php
+++ database/migrations/2026_03_05_112033_rename_status_to_is_active_in_program_requirement_categories_table.php
@@ -6,27 +6,13 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
-    public function up(): void
-    {
-        // Optional: convert old string values to 1/0 first
-        DB::table('program_requirement_categories')
-            ->where('status', 'draft')
-            ->update(['status' => 0]);
 
-        DB::table('program_requirement_categories')
-            ->where('status', 'published')
-            ->update(['status' => 1]);
-
-        Schema::table('program_requirement_categories', function (Blueprint $table) {
-            // Change type to boolean
-            $table->boolean('status')->default(1)->change();
-        });
-
-        Schema::table('program_requirement_categories', function (Blueprint $table) {
-            // Rename column
-            $table->renameColumn('status', 'is_active');
-        });
-    }
+public function up(): void
+{
+    Schema::table('program_requirement_categories', function (Blueprint $table) {
+        $table->renameColumn('status', 'is_active');
+    });
+}
 
     public function down(): void
     {
